a8d1a713b50977868b33647aa7d83afa2c4387bb
Mahfuz Ahmed <ahmed.mahfuz.232@northsouth.edu>
2024-09-01 03:26:26 +0600

    fourth

--- auth.js
+++ auth.js
@@ -4,7 +4,6 @@ function getCookie(name) {
       const cookies = document.cookie.split(';');
       for (let i = 0; i < cookies.length; i++) {
           const cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
           if (cookie.substring(0, name.length + 1) === (name + '=')) {
               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
               break;
@@ -48,7 +47,7 @@ const handleRegistration = (event) => {
       last_name: last_name,
       email: email,
       password: password,
-      password2: password2, // Include both password and password2
+      password2: password2, 
     }),
   })
     .then((response) => {
